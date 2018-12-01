1db2a5c94c2481433b8bdb916efd3fb7cd887bc8
nypzxy <nypzxy@gmail.com>
2018-12-01 23:50:08 +1100

    init repos

---
+++ python/LinearRegression/LinearRegression.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+import numpy as np
+
+
+# 加载txt和csv文件
+def loadtxtAndcsv_data(fileName, split, dataType):
+    print(u"加载数据...\n")
+
+
+def linearRegression(alpha=0.01, num_iters=400):
+    print(u"加载数据...\n")
+
+
+# 测试linearRegression函数
+def testLinearRegression():
+    linearRegression(0.01, 400)
+    # print u"\n计算的theta值为：\n",theta
+    # print u"\n预测结果为：%f"%predict(mu, sigma, theta)
+
+
+# 梯度下降算法
+if __name__ == "__main__":
+    testLinearRegression()
